1c4ea7f4df1c7cf11ec6bc39c173fc4ae3b00439
Tharsinaa <tharsinaagunalan@gmail.com>
2024-09-20 12:19:36 +0800

    Add project files.

---
+++ Oop Week3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //creating an object (variable of a cetain class type)
+        Transaction transaction = new Transaction();
+
+        transaction.setDate(new DateTime(2008, 6, 1));
+        transaction.setVal(10.5f);
+
+        Console.WriteLine("Transaction value is " + transaction.getVal() +
+            " at time " + transaction.getDate());
+    }
+
+}
---
+++ Oop Week3/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ public class Transaction
+{
+    private float val;
+    private DateTime date;
+
+    // setter & getter
+    public void setVal(float val)
+    {
+        if (val > 0) this.val = val;
+    }
+    public void setDate(DateTime date) { this.date = date; }
+    public float getVal() { return this.val; }
+    public DateTime getDate() { return this.date; }
+}
